36fbc1f466b917d5630588946979e89132b6243e
Ebony Nicholas <1ebonyn1@gmail.com>
2025-03-12 11:36:23 +1100

    Moved get_xml function to main test file for easy use in other tests

--- src/tests/test_upload_order.py
+++ src/tests/test_upload_order.py
@@ -4,17 +4,13 @@ from fastapi.testclient import TestClient
 from pathlib import Path
 
 from src import main
+from tests.tests_main import get_xml
 app = main.app
 
 client = TestClient(app)
-
-def get_order_doc_xml(file_name):
-	valid_xml_file_path = Path(__file__).parent / "resources" / file_name
-	with valid_xml_file_path.open("r", encoding="utf-8") as xml_file:
-		return xml_file.read()
      
 def test_upload_order_doc_valid():
-    ubl_xml_content = get_order_doc_xml("order_provided_valid.xml")
+    ubl_xml_content = get_xml("order_provided_valid.xml")
     files = {"file": ("valid_order_doc.xml", ubl_xml_content, "text/xml")}
     response = client.post("/ubl/order/upload", files=files)
     assert response.status_code == 200
@@ -32,21 +28,21 @@ def test_upload_order_doc_empty_file():
     #assert response.json()["detail"] == "Empty file"
 
 def test_upload_order_doc_empty_id():
-    ubl_xml_content = get_order_doc_xml("order_provided_empty_id.xml")
+    ubl_xml_content = get_xml("order_provided_empty_id.xml")
     files = {"file": ("no_id_order_doc.xml", ubl_xml_content, "text/xml")}
     response = client.post("/ubl/order/upload", files=files)
     assert response.status_code == 400
     #assert response.json()["detail"] == "Order ID not found"
 
 def test_upload_order_doc_no_id():
-    ubl_xml_content = get_order_doc_xml("order_provided_no_id.xml")
+    ubl_xml_content = get_xml("order_provided_no_id.xml")
     files = {"file": ("empty_id_order_doc.xml", ubl_xml_content, "text/xml")}
     response = client.post("/ubl/order/upload", files=files)
     assert response.status_code == 400
     #assert response.json()["detail"] == "Order ID not found"
 
 def test_upload_order_doc_invalid_xml():
-    ubl_xml_content = get_order_doc_xml("order_provided_invalid.xml")
+    ubl_xml_content = get_xml("order_provided_invalid.xml")
     files = {"file": ("invalid_xml_order_doc.xml", ubl_xml_content, "text/xml")}
     response = client.post("/ubl/order/upload", files=files)
     assert response.status_code == 400 
---
+++ src/tests/tests_main.py
@@ -0,0 +1,9 @@
+import io
+import os
+from fastapi.testclient import TestClient
+from pathlib import Path
+
+def get_xml(file_name):
+	valid_xml_file_path = Path(__file__).parent / "resources" / file_name
+	with valid_xml_file_path.open("r", encoding="utf-8") as xml_file:
+		return xml_file.read()
