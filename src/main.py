99102ae31b81b8d593b0f93d2cadc4ec10babc30
Benny Wu <z5604774@ad.unsw.edu.au>
2025-03-14 19:34:12 +1100

    Created implementation for parsing

--- src/main.py
+++ src/main.py
@@ -6,7 +6,9 @@ Returns:
     _type_: _description_
 """
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+import json
+import xmltodict
 
 app = FastAPI()
 
@@ -21,3 +23,24 @@ def index():
         _type_: _description_
     """
     return {"details": "Hello, World!"}
+
+@app.post("/ubl/order/parse")
+async def parse_ubl_order(file: UploadFile = File(...)):
+    if file is None:
+        raise HTTPException(status_code=400, detail="No file provided")
+    try:
+        xml_content = await file.read()
+
+        data_dict = xmltodict.parse(xml_content, process_namespaces=False)
+
+        json_data = json.dumps(data_dict, indent=4)
+
+        # This uncommented code turns the JSON data into a file
+        # with open("data.json", "w") as json_file:
+        #     json_file.write(json_data)
+        
+        # return json.loads(json_data)
+
+        return json_data
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Invalid XML file")
