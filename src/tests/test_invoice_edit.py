57065ce8352809c64c28629b971fdf25f81e95bd
Archi <z5449011@ad.unsw.edu.au>
2025-03-15 05:12:51 +1100

    added docstyle

--- src/tests/test_invoice_edit.py
+++ src/tests/test_invoice_edit.py
@@ -1,3 +1,11 @@
+"""_summary_
+
+contains route tests
+
+Returns:
+    _type_: _description_
+"""
+
 import json
 import os
 from fastapi.testclient import TestClient
@@ -9,7 +17,12 @@ INVOICE_FILE = "invoice_data.json"
 
 @pytest.fixture(scope="function", autouse=True)
 def setup_invoice_file():
-    # Setup: Create a sample invoice file
+    """_summary_
+
+    invoice file setup
+    """
+
+    # Setup sample invoice file
     sample_data = {
         "123": {
             "invoiceId": "123",
@@ -22,16 +35,19 @@ def setup_invoice_file():
         }
     }
 
-    with open(INVOICE_FILE, "w") as file:
+    with open(INVOICE_FILE, "w", encoding="utf-8") as file:
         json.dump(sample_data, file)
 
     yield
 
-    # Teardown: Remove the file after test execution
     if os.path.exists(INVOICE_FILE):
         os.remove(INVOICE_FILE)
 
 def test_edit_invoice_success():
+    """_summary_
+
+    testing successful edit in invoice
+    """
     updated_data = {"customer": "Updated Customer", "total": {"amt": "150.00", "cur": "USD"}}
 
     response = client.put("/ubl/invoice/edit/123", json=updated_data)
@@ -43,21 +59,33 @@ def test_edit_invoice_success():
     assert response.json()["total"]["cur"] == "USD"
 
 def test_edit_invoice_not_found():
+    """_summary_
+
+    Error: invoice not found
+    """
     response = client.put("/ubl/invoice/edit/999", json={"customer": "New Customer"})
 
     assert response.status_code == 400
     assert response.json() == {"detail": "Invoice not found"}
 
 def test_edit_invoice_missing_input():
+    """_summary_
+
+    Error: invoice has missing inputs
+    """
     response = client.put("/ubl/invoice/edit/123", json={})
 
     assert response.status_code == 400
     assert response.json() == {"detail": "Missing or invalid input data"}
 
 def test_edit_invoice_file_not_found():
+    """_summary_
+
+    Error: invoice file not found
+    """
     os.remove(INVOICE_FILE)
 
     response = client.put("/ubl/invoice/edit/123", json={"customer": "New Customer"})
 
     assert response.status_code == 500
-    assert response.json() == {"detail": "Internal server error: Invoice file not found"}
+    assert response.json() == {"detail": "Invoice file not found"}
