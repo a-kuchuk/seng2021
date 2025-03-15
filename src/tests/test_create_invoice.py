f0d388e35fd13b894e960e7a57749e012fa0e067
Caitlin <caitlin.wong@student.unsw.edu.au>
2025-03-15 14:41:35 +1100

    adding create invoice to proper place

--- src/tests/test_create_invoice.py
+++ src/tests/test_create_invoice.py
@@ -1,11 +1,15 @@
+"""This Module is for testing the creation of an XML Invoice file"""
+
 import json
 import os
 from fastapi.testclient import TestClient
-from main import app  
+from src import main
 
+app = main.app
+ 
 client = TestClient(app)
-
 def test_create_invoice_success():
+    """Tests if an invoice can be created successfully from a JSON file"""
     json_data = {
         "JSONId": "123",
         "date": "2011-09-22",
@@ -31,21 +35,21 @@ def test_create_invoice_success():
     os.remove("data.json")
     os.remove("invoice.xml")
     print("invoice creation success!")
-
+    
 def test_create_invoice_empty_json():
+    """Tests the creation of Invoice from Empty JSON file"""
     with open("data.json", "w") as file:
         file.write("{}")
     
     response = client.post("/ubl/invoice/create/123")
-
     assert response.status_code == 400 
     assert "File is Empty" in response.json()["detail"] 
-
     
     os.remove("data.json")
     print("invoice empty success!")
-
+    
 def test_create_invoice_file_not_found():
+    """Tests the creation of Invoice file from non-existing JSON file"""
     if os.path.exists("data.json"):
         os.remove("data.json")
     
@@ -54,7 +58,6 @@ def test_create_invoice_file_not_found():
     assert response.status_code == 400 
     assert "File doesn't exist" in response.json()["detail"]
     print("success not found!")
-
 if __name__ == "__main__":
     test_create_invoice_success()
     test_create_invoice_empty_json()
