89e684a540df08e0a7a2ba6c02056d55b383374f
Benny Wu <z5604774@ad.unsw.edu.au>
2025-03-16 01:33:38 +1100

    Added implementation for API. Did refine in the same function as it is more efficient

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ Returns:
 """
 
 import json
-from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi import FastAPI, UploadFile, File, HTTPException, Body
 import xmltodict
 
 app = FastAPI()
@@ -52,3 +52,73 @@ async def parse_ubl_order(file: UploadFile = File(...)):
         return json_data
     except Exception as e:
         raise HTTPException(status_code=400, detail="Invalid XML file") from e
+
+
+@app.post("/ubl/order/validate")
+async def validate_order(order_JSON: str = Body(...)):
+    try:
+        if order_JSON is None:
+            raise HTTPException(status_code=400, detail="No JSON provided")
+
+        order_data = json.loads(order_JSON)
+        order_data = order_data.get("Order", {})
+        refined_order = {
+            "InvoiceID": {"ID": (id(object()) * 13) % 100 + 1},
+            "IssueDate": order_data.get("cbc:IssueDate"),
+            "InvoicePeriod": {
+                "StartDate": order_data.get("cac:Delivery", {})
+                                    .get("cac:RequestedDeliveryPeriod", {})
+                                    .get("cbc:StartDate"),
+                "EndDate": order_data.get("cac:Delivery", {})
+                                    .get("cac:RequestedDeliveryPeriod", {})
+                                    .get("cbc:EndDate")
+            },
+            "AccountingSupplierParty": {
+                "Name": order_data.get("cac:SellerSupplierParty", {})
+                                .get("cac:Party", {})
+                                .get("cac:PartyName", {})
+                                .get("cbc:Name")
+            },
+            "AccountingCustomerParty": {
+                "Name": order_data.get("cac:BuyerCustomerParty", {})
+                                .get("cac:Party", {})
+                                .get("cac:PartyName", {})
+                                .get("cbc:Name")
+            },
+            "LegalMonetaryTotal": {
+                "PayableAmount": {
+                    "Value": order_data.get("cac:AnticipatedMonetaryTotal", {})
+                                    .get("cbc:PayableAmount", {})
+                                    .get("#text"),
+                    "Currency": order_data.get("cac:AnticipatedMonetaryTotal", {})
+                                        .get("cbc:PayableAmount", {})
+                                        .get("@currencyID")
+                }
+            },
+            "InvoiceLine": {
+                "ID": order_data.get("cac:OrderLine", {})
+                                .get("cac:LineItem", {})
+                                .get("cbc:ID"),
+                "Amount": {
+                    "Value": order_data.get("cac:OrderLine", {})
+                                    .get("cac:LineItem", {})
+                                    .get("cbc:LineExtensionAmount", {})
+                                    .get("#text"),
+                    "Currency": order_data.get("cac:OrderLine", {})
+                                        .get("cac:LineItem", {})
+                                        .get("cbc:LineExtensionAmount", {})
+                                        .get("@currencyID")
+                },
+                "Item": {
+                    "Description": order_data.get("cac:OrderLine", {})
+                                            .get("cac:LineItem", {})
+                                            .get("cac:Item", {})
+                                            .get("cbc:Description")
+                }
+            }
+        }
+
+        return {"validatedOrder": refined_order}
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Invalid JSON data") from e
