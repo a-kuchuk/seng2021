ae10ca5cc6185ec62a2773123ff4170664178fc8
Caitlin <caitlin.wong@student.unsw.edu.au>
2025-03-16 23:42:55 +1100

    invoice pdf impl and a simple test

--- src/tests/tests_main.py
+++ src/tests/tests_main.py
@@ -4,5 +4,5 @@ from fastapi.testclient import TestClient
 from src import main
 
 def test_xml_to_pdf(): 
-    response = client.get("/ubl/invoice/pdf/invoice_provided.xml")
+    response = client.get("/ubl/invoice/pdf/invoice_provided_valid.xml")
 
