476dd216bc03a08dc422d4f54e75bb38b7643853
Ebony Nicholas <1ebonyn1@gmail.com>
2025-03-17 12:01:08 +1100

    Added mimetype to requirements

--- src/tests/test_upload_order.py
+++ src/tests/test_upload_order.py
@@ -1,25 +1,27 @@
 """Module to test the upload of an order document."""
 
 import io
+from pathlib import Path
 from fastapi.testclient import TestClient
 
-#from src import main
+# from src import main
 from src.main import app
-#from src.tests.tests_main import get_xml
-from pathlib import Path
+
+# from src.tests.tests_main import get_xml
 
 
 
-
-#app = main.app
+# app = main.app
 
 client = TestClient(app)
+
 
 def get_xml(file_name):
     """Function to read the content of an XML file."""
     valid_xml_file_path = Path(__file__).parent / "resources" / file_name
     with valid_xml_file_path.open("r", encoding="utf-8") as xml_file:
         return xml_file.read()
+
 
 def test_upload_order_doc_valid():
     """Test the upload of a valid order document."""
@@ -75,4 +77,4 @@ def test_upload_order_doc_non_xml():
     files = {"file": ("non_xml_order_doc.txt", non_xml_file, "text/plain")}
     response = client.post("/ubl/order/upload", files=files)
     assert response.status_code == 400
-    #assert response.json()["detail"] == "File must be an XML file"
+    assert response.json()["detail"] == "File must be an XML file"
